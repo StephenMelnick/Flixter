10220342ba74185404a79d2c662bd7822901979a
Stephen <stephenmelnick8@gmail.com>
2019-07-15 23:18:26 +0000

    finalizing permissoins

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,6 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
-
+  before_action :require_authorized_for_current_course, only: [:show]
   def new
   @course = Course.new
   end
@@ -15,14 +15,22 @@ else
 end
 
 def show
-  @course = Course.find(params[:id])
-end
+  end
 
 private
 
-def course_params
-  params.require(:course).permit(:title, :description, :cost)
+def require_authoirized_for_current_course
+  if current_course.user != current_user
+    render plain: "Unauthorized", status: :unauthorized
+  end
 end
 
+helper_method :current_course
+def current_course
+  @current_course ||= Course.find(params[:id])
+end
 
+def course_params
+  params.require(:course).permit(:title, :description, :cost)
+  end
 end
