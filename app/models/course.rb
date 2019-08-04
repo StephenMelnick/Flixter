a27c702eb583bed1864abb6016714ef3109cc5bf
Stephen <stephenmelnick8@gmail.com>
2019-08-04 20:40:00 +0000

    image can't be blank

--- app/models/course.rb
+++ app/models/course.rb
@@ -8,6 +8,7 @@ has_many :enrollments
 
 validates :title, presence: true
 validates :description, presence: true
+validates :image, presence: true
 validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 
   def free?
